eba9aba06c2ec81a8e8aa1e91d6948961fac3add
Astrid96 <81070831+Astri96@users.noreply.github.com>
2024-11-09 20:47:32 +0700

    Update pinterest.py

--- api/pinterest.py
+++ api/pinterest.py
@@ -51,14 +51,14 @@ def pinterest():
         return jsonify({
             "status": 503,
             "creator": "Astri",
-            "error": f"Service is unavailable: {str(e)}"
+            "error": f"Service is unavailable"
         }), 503
 
     except Exception as e:
         return jsonify({
             "status": 500,
             "creator": "Astri",
-            "error": f"An unexpected error occurred: {str(e)}"
+            "error": f"An unexpected error occurred"
         }), 500
 
 if __name__ == "__main__":
