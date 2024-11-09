5b92b9390ebafe972b1349cf85b73b13c5b4c9fb
Astrid96 <81070831+Astri96@users.noreply.github.com>
2024-11-09 13:16:18 +0700

    Update photo2anime2.py

--- api/photo2anime2.py
+++ api/photo2anime2.py
@@ -8,10 +8,10 @@ import tempfile
 
 app = Flask(__name__)
 
-# Public directory for serving images
-PUBLIC_IMAGE_DIR = './tmp/temp_images'
+# Use /tmp directory on Vercel for storing images (Writable directory)
+PUBLIC_IMAGE_DIR = '/tmp/temp_images'
 
-# Ensure the public directory exists
+# Ensure the temporary directory exists
 Path(PUBLIC_IMAGE_DIR).mkdir(parents=True, exist_ok=True)
 
 @app.route('/api/photo2anime2', methods=['GET'])
@@ -32,11 +32,11 @@ def photo2anime2():
         # Open the image from the response content
         img = Image.open(BytesIO(response.content))
 
-        # Define a public image file path
+        # Define a temporary image file path
         img_name = os.path.basename(image_url)
         img_path = os.path.join(PUBLIC_IMAGE_DIR, img_name)
 
-        # Save the image to the public directory
+        # Save the image to the temporary directory
         img.save(img_path)
 
         # Construct the served image URL based on your domain
@@ -54,3 +54,4 @@ def photo2anime2():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
