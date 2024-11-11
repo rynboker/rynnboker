79b10e2cb9f62ce0bea70653134e1ed934c00e14
Astrid96 <81070831+Astri96@users.noreply.github.com>
2024-11-11 08:19:49 +0700

    Update gempa.py

--- api/gempa.py
+++ api/gempa.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/api/gempa', methods=['GET'])
 def gempa():
 
-    # Call the external image search API
+    # Call the external earthquake data API
     api_url = f"https://api.agatz.xyz/api/gempa"
     try:
         response = requests.get(api_url)
@@ -19,23 +19,30 @@ def gempa():
                 "error": "Sorry, an error occurred with our external service. Please try again later."
             }), response.status_code
 
-        # Extract and format the data as needed
-        external_data = response.json().get("data", [])
-        formatted_data = [
-            {
-                "date": item.get("tanggal"),
-                "time": item.get("waktu"),
-                "potential": item.get("potensi"),
-                "magnitude": item.get("magnitude"),
-                "depth": item.get("kedalaman"),
-                "region": item.get("wilayah"),
-                "latitude": item.get("lintang"),
-                "oblong": item.get("bujur"),
-                "coordinate": item.get("koordinat"),
-                "felt": item.get("dirasakan")
-            }
-            for item in external_data
-        ]
+        # Extract and format the data from the external API response
+        external_data = response.json().get("data", {})
+
+        # Check if external data is present
+        if not external_data:
+            return jsonify({
+                "status": 404,
+                "creator": "Astri",
+                "error": "No earthquake data found."
+            }), 404
+
+        # Format the data as needed
+        formatted_data = {
+            "date": external_data.get("tanggal"),
+            "time": external_data.get("waktu"),
+            "potential": external_data.get("potensi"),
+            "magnitude": external_data.get("magnitude"),
+            "depth": external_data.get("kedalaman"),
+            "region": external_data.get("wilayah"),
+            "latitude": external_data.get("lintang"),
+            "longitude": external_data.get("bujur"),
+            "coordinates": external_data.get("koordinat"),
+            "felt": external_data.get("dirasakan")
+        }
 
         return jsonify({
             "status": 200,
@@ -47,14 +54,14 @@ def gempa():
         return jsonify({
             "status": 503,
             "creator": "Astri",
-            "error": f"Service is unavailable"
+            "error": "Service is unavailable"
         }), 503
 
     except Exception as e:
         return jsonify({
             "status": 500,
             "creator": "Astri",
-            "error": f"An unexpected error occurred"
+            "error": "An unexpected error occurred"
         }), 500
 
 if __name__ == "__main__":
