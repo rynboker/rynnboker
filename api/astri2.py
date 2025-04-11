0b395605664ad15c5ffaeec1ab2f8070424f894b
AmeLia <81070831+nomel2837@users.noreply.github.com>
2025-04-11 10:27:51 +0700

    Update astri2.py

--- api/astri2.py
+++ api/astri2.py
@@ -41,7 +41,7 @@ def index():
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
-<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <style>
 html {
   height: 100%;
