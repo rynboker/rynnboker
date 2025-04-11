f459b59f8273707ccbe6cd340446418d409c452a
AmeLia <81070831+nomel2837@users.noreply.github.com>
2025-04-11 10:13:39 +0700

    Update astri2.py

--- api/astri2.py
+++ api/astri2.py
@@ -33,39 +33,95 @@ def generate_short_code(length=6):
 @app.route("/")
 def index():
     html = """
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <title>Welcome</title>
-        <style>
-            body {
-                background-color: #f0f0f0;
-                font-family: Arial, sans-serif;
-                color: #333;
-                text-align: center;
-                margin-top: 50px;
-            }
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Putri Amelia</title>
+  <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
+  <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
+  <link rel="stylesheet" href="/amel.css">
+<style>
 
-            h1 {
-                color: #007acc;
-            }
+</style>
+</head>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false' style='-moz-user-select: none; cursor: default;'>
+<!-- partial:index.partial.html -->
+<aside class="profile-card">
+  <header>
+    <!-- here’s the avatar -->
+    <a target="_blank" href="#">
+      <img src="https://avatars.githubusercontent.com/u/81070831?v=4" class="hoverZoomLink">
+    </a>
 
-            a {
-                color: #ff4081;
-                text-decoration: none;
-            }
+    <!-- the username -->
+    <h1>
+            Putri Amelia
+          </h1>
 
-            a:hover {
-                text-decoration: underline;
-            }
-        </style>
-    </head>
-    <body>
-        <h1>Selamat Datang di siastri.my.id</h1>
-        <p>Ini adalah halaman utama.</p>
-        <a href="/api/shorturl">Lihat API ShortURL</a>
-    </body>
-    </html>
+    <!-- and role or location -->
+    <h2>
+            A.D.I Family
+          </h2>
+
+  </header>
+
+  <!-- bit of a bio; who are you? -->
+  <div class="profile-bio">
+
+    <p>
+      My name is Putri Amelia, you can call me Lia. I was born in Bandung, Indonesia. I'm 24 years old and still in collage. This is an API website, you can join my discord server.
+    </p>
+
+  </div>
+
+  <!-- some social links to show off -->
+  <ul class="profile-social-links">
+    <li>
+      <a target="_blank" href="https://github.com/nomel2837">
+        <i class="fa fa-github"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://discord.com/users/826098988725829662">
+        <i class="fa fa-user"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://instagram.com/_matchaamel">
+        <i class="fa fa-instagram"></i>
+      </a>
+    </li>
+    <li>
+      <a target="_blank" href="https://discord.gg/nHm2ZwvqC8">
+       
+        <i class="fa fa-link"></i>
+      </a>
+    </li>
+  </ul>
+</aside>
+<!-- partial -->
+  
+</body>
+<script type='text/javascript'>
+  shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};
+  if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);
+  else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;
+  if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);
+  if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");
+  var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},
+  i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,
+  "break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,
+  f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);
+  for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:
+  c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))
+  return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},
+  this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:
+  function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;
+  c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://sahretech.com"});
+</script>
+</html>
     """
     return Response(html, mimetype='text/html')
 
