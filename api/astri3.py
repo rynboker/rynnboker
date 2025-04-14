b3854470a56c8b195d2f319ebb6f48a648ecb24b
AmeLia <81070831+nomel2837@users.noreply.github.com>
2025-04-14 09:13:14 +0700

    Update astri3.py

--- api/astri3.py
+++ api/astri3.py
@@ -4,6 +4,98 @@ import logging
 
 app = Flask(__name__)
 
+@app.route("/status")
+def status():
+    html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>API Status - siastri.my.id</title>
+  <script>
+    const apis = [
+      "/api/photo2anime2", "/api/gempa", "/api/gimage", "/api/pinterest", "/api/pinterest2",
+      "/api/gptlogic", "/api/shorturl", "/api/spotify", "/api/threads", "/api/translate",
+      "/api/ttsstalk", "/api/ytsearch", "/api/ytplayvid", "/api/ytplayaud", "/api/otakudesu",
+      "/api/otakudesulatest", "/api/soundcloud", "/api/soundclouddl", "/api/twitter",
+      "/api/twitterstalk", "/api/spotifydl", "/api/weather", "/api/corona/indonesia",
+      "/api/corona/global", "/api/minecraft", "/api/ppcouple", "/api/ytchannel",
+      "/api/youtube/transcript", "/api/musicsearch", "/api/random_neko", "/api/removebg",
+      "/api/upscale", "/api/bubblechat", "/api/nulis", "/api/welcome", "/api/ktp", "/api/rank",
+      "/api/leave", "/api/qrcode", "/api/diffusion", "/api/wmit", "/api/wait", "/api/nhentai",
+      "/api/nhentaisearch", "/api/gimage2", "/api/ouo", "/api/tiktokmusic", "/api/tiktoknowm",
+      "/api/telestick", "/api/growikibeta", "/api/simi", "/api/instagram", "/api/ocr",
+      "/api/character", "/api/anime", "/api/wiki"
+    ];
+
+    async function checkStatus() {
+      const list = document.getElementById("api-list");
+      for (const endpoint of apis) {
+        const li = document.createElement("li");
+        li.textContent = `${endpoint} - Checking...`;
+        list.appendChild(li);
+
+        try {
+          const res = await fetch(endpoint, { method: "GET" });
+          if (res.ok) {
+            li.innerHTML = `<a href="${endpoint}" target="_blank">${endpoint}</a> - ✅ Online`;
+          } else {
+            li.innerHTML = `<a href="${endpoint}" target="_blank">${endpoint}</a> - ❌ Error (${res.status})`;
+          }
+        } catch (err) {
+          li.innerHTML = `<a href="${endpoint}" target="_blank">${endpoint}</a> - ❌ Offline`;
+        }
+      }
+    }
+
+    window.onload = checkStatus;
+  </script>
+  <style>
+    body {
+      font-family: sans-serif;
+      background-color: #000;
+      color: #fff;
+      padding: 2rem;
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+    li {
+      margin-bottom: 8px;
+    }
+    a {
+      color: #00bfff;
+      text-decoration: none;
+    }
+  </style>
+</head>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false' style='-moz-user-select: none; cursor: default;'>
+  <h1>API Status - siastri.my.id</h1>
+  <p>Berikut daftar API yang tersedia dan statusnya:</p>
+  <ul id="api-list"></ul>
+</body>
+<script type='text/javascript'>
+  shortcut={all_shortcuts:{},add:function(a,b,c){var d={type:"keydown",propagate:!1,disable_in_input:!1,target:document,keycode:!1};
+  if(c)for(var e in d)"undefined"==typeof c[e]&&(c[e]=d[e]);
+  else c=d;d=c.target,"string"==typeof c.target&&(d=document.getElementById(c.target)),a=a.toLowerCase(),e=function(d){d=d||window.event;
+  if(c.disable_in_input){var e;d.target?e=d.target:d.srcElement&&(e=d.srcElement),3==e.nodeType&&(e=e.parentNode);
+  if("INPUT"==e.tagName||"TEXTAREA"==e.tagName)return}d.keyCode?code=d.keyCode:d.which&&(code=d.which),e=String.fromCharCode(code).toLowerCase(),188==code&&(e=","),190==code&&(e=".");
+  var f=a.split("+"),g=0,h={"`":"~",1:"!",2:"@",3:"#",4:"$",5:"%",6:"^",7:"&",8:"*",9:"(",0:")","-":"_","=":"+",";":":","'":'"',",":"<",".":">","/":"?","\\":"|"},
+  i={esc:27,escape:27,tab:9,space:32,"return":13,enter:13,backspace:8,scrolllock:145,scroll_lock:145,scroll:145,capslock:20,caps_lock:20,caps:20,numlock:144,num_lock:144,num:144,pause:19,
+  "break":19,insert:45,home:36,"delete":46,end:35,pageup:33,page_up:33,pu:33,pagedown:34,page_down:34,pd:34,left:37,up:38,right:39,down:40,f1:112,f2:113,f3:114,f4:115,f5:116,f6:117,f7:118,
+  f8:119,f9:120,f10:121,f11:122,f12:123},j=!1,l=!1,m=!1,n=!1,o=!1,p=!1,q=!1,r=!1;d.ctrlKey&&(n=!0),d.shiftKey&&(l=!0),d.altKey&&(p=!0),d.metaKey&&(r=!0);
+  for(var s=0;k=f[s],s<f.length;s++)"ctrl"==k||"control"==k?(g++,m=!0):"shift"==k?(g++,j=!0):"alt"==k?(g++,o=!0):"meta"==k?(g++,q=!0):1<k.length?i[k]==code&&g++:
+  c.keycode?c.keycode==code&&g++:e==k?g++:h[e]&&d.shiftKey&&(e=h[e],e==k&&g++);if(g==f.length&&n==m&&l==j&&p==o&&r==q&&(b(d),!c.propagate))
+  return d.cancelBubble=!0,d.returnValue=!1,d.stopPropagation&&(d.stopPropagation(),d.preventDefault()),!1},
+  this.all_shortcuts[a]={callback:e,target:d,event:c.type},d.addEventListener?d.addEventListener(c.type,e,!1):d.attachEvent?d.attachEvent("on"+c.type,e):d["on"+c.type]=e},remove:
+  function(a){var a=a.toLowerCase(),b=this.all_shortcuts[a];delete this.all_shortcuts[a];if(b){var a=b.event,c=b.target,b=b.callback;
+  c.detachEvent?c.detachEvent("on"+a,b):c.removeEventListener?c.removeEventListener(a,b,!1):c["on"+a]=!1}}},shortcut.add("Ctrl+U",function(){top.location.href="http://sahretech.com"});
+</script>
+</html>
+"""
+return Response(html, mimetype='text/html')
+
 # API /api/ytsearch
 @app.route('/api/ytsearch', methods=['GET'])
 def ytsearch():
