cef85f708bc24fce360969b849f35105d8994a6b
rynboker <rynboker@gmail.com>
2025-10-13 23:37:06 +0700

    Update astri2.py

--- api/astri2.py
+++ api/astri2.py
@@ -37,7 +37,7 @@ def index():
 <html lang="en" >
 <head>
   <meta charset="UTF-8">
-  <title>Putri Amelia</title>
+  <title>Hi Riyan</title>
   <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto'>
   <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
 <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css'>
